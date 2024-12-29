002e9937ffa218d15bcd2ea21c19577bc71cb783
Cameron Cooper <coopercc06@gmail.com>
2024-12-28 21:12:38 -0600

    double fault handling

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,42 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(arcos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
+use arcos::println;
 
-mod vga;
+static _HELLO_WORLD: &[u8] = b"Hello, World!";
 
-static HELLO_WORLD: &[u8] = b"Hello, World!";
-
+#[allow(clippy::cfg_not_test)]
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    arcos::test_panic_handler(info);
+}
+
 /* vga buff is 25x80 */
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("RAHHH THIS IS MY TIME TO SHINE BITCH");
 
+    arcos::init();
+
+
     #[cfg(test)]
     test_main();
+
+    println!("It crashed, how the hell can you even see this message");
 
     loop {}
 }
 
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-}
 
-#[test_case]
-fn assertion() {
-    print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    println!("[ok]")
-}
