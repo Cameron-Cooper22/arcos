f305d728e59c029881f0bc83a69682ab5d2b8a07
Cameron Cooper <coopercc06@gmail.com>
2025-01-08 15:23:34 -0600

    vga
    implements basic text output, next step will be implementing the IDT and
    handling keyboard input. QEMU will emulate the keyboard as PS/2, but USB
    drivers will meed to be added to further add on to it

--- Makefile
+++
@@ -1,8 +0,0 @@
-.PHONY: clean, .force-rebuild
-	all: bootloader.bin
-
-bootloader.bin: os.asm .force-rebuild
-	i686-elf-as -fbin boot.s -o arcos.bin
-
-clean: 
-	rm *.bin
--- boot/boot.asm
+++ boot/boot.asm
@@ -1,48 +1,27 @@
-;
-; The GDT
-;
-; boot.asm
-;
+[org 0x7c00]
 
-; Set Program Origin
-[org 0x7C00]
+jmp begin_real
 
-; 16-bit Mode
-[bits 16]
+kernel_size db 0
 
-; Initialize the base pointer and the stack pointer
-; The initial values should be fine for what we've done so far,
-; but it's better to do it explicitly
-mov bp, 0x0500
-mov sp, bp
+begin_real:
+  bits 16
 
-; Before we do anything else, we want to save the ID of the boot
-; drive, which the BIOS stores in register dl. We can offload this
-; to a specific location in memory
-mov byte[boot_drive], dl
+  mov bp, 0x0500
+  mov sp, bp
 
-mov bx, msg_hello_world
-call print_bios
+  mov byte[boot_drive], dl
 
-; Load the next sector
+  mov bx, 0x0002
 
-; The first sector's already been loaded, so we start with the second sector
-; of the drive. Note: Only bl will be used
-mov bx, 0x0002
+  mov cx, [kernel_size]
+  add cx, 2
 
-; We only want to load one sector from the disk for now. This will
-; be higher later. Note: Only cl will be used
-mov cx, 0x0001
+  mov dx, 0x7e00
+  
+  call load_bios
 
-; Finally, we want to store the new sector immediately after the first
-; loaded sector, at adress 0x7E00. This will help a lot with jumping between
-; different sectors of the bootloader.
-mov dx, 0x7E00
-
-call load_bios
-
-; To 32-bit
-call elevate_bios
+  call elevate_bios
 
 bootsector_hold:
   jmp $
@@ -53,23 +32,24 @@ bootsector_hold:
 %include "real_mode/gdt.asm"
 %include "real_mode/elevate.asm"
 
-msg_hello_world:                db `\r\nHello World, from the BIOS!\r\n`, 0
-
-boot_drive:                     db 0x00
+boot_drive:		db 0x00
 
 times 510 - ($ - $$) db 0x00
 
-dw 0xAA55
+dw 0xaa55
 
 bootsector_extended:
 begin_protected:
+
+  bits 32
 
   call clear_protected
 
   call detect_lm_protected
 
-  mov esi, protected_alert
-  call print_protected
+  call init_pt_protected
+
+  call elevate_protected
 
   jmp $
 
@@ -77,17 +57,34 @@ begin_protected:
 %include "protected_mode/print.asm"
 %include "protected_mode/detect_lm.asm"
 %include "protected_mode/init_pt.asm"
+%include "protected_mode/gdt.asm"
+%include "protected_mode/elevate.asm"
 
 vga_start:		equ 0x000b8000
-vga_extent:		equ 80 * 25 * 2	; 80 (w) * 25 (h) * 2 (bytes)
-kernel_start:		equ 0x00100000
+vga_extent:		equ 80 * 25 * 2
 style_wb:		equ 0x0f
 
-protected_alert:                 db `64-bit long mode supported`, 0
-
 times 512 - ($ - bootsector_extended) db 0x00
-long_mode_init:
+begin_long_mode:
+  bits 64
+
+  mov rdi, style_blue
+  call clear_long
+  
+  mov rdi, style_blue
+  mov rsi, long_mode_note
+  call print_long
 
 
+  call kernel_start
 
-times 512 - ($ - long_mode_init) db 0x00
+  jmp $
+
+%include "long_mode/clear.asm"
+%include "long_mode/print.asm"
+
+kernel_start:                   equ 0x8200              ; Kernel is at 1MB
+long_mode_note:                 db `Now running in fully-enabled, 64-bit long mode!`, 0
+style_blue:                     equ 0x1F
+
+times 512 - ($ - begin_long_mode) db 0x00
