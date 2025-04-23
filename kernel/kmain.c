108389aca336aec6aa4f76625552d37dc31674ff
Cameron Cooper <coopercc06@gmail.com>
2025-04-23 07:23:03 -0500

    still not working, but build system in place

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,15 +1,16 @@
 #include <memory.h>
+#include <stdint.h>
 
 void clear_screen() {
-  unsigned char *vga_mem = P2V(0xb8000);
+  uintptr_t *vga_mem = (unsigned long *)P2V(0xb8000);
   for (int i = 0; i < 80 * 24 * 2; i++) *vga_mem++ = 0;
 }
 
 void print_string(char* str) {
-  unsigned char *vga_mem = P2V(0xB8000);
+  uintptr_t *vga_mem = (uintptr_t *)P2V(0xB8000);
   while (*str) {
-    *vidmem++ = *str++;
-    *vidmem++ = 0x7;	// 0b0111
+    *vga_mem++ = *str++;
+    *vga_mem++ = 0x7;	// 0b0111
   }
 }
 
